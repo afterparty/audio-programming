89878e714c6d0ba9cd1ac9f59c914feca634d6ba
afterparty <char6447@yahoo.com>
2019-04-23 22:07:27 +0000

    beginning of 1.9 program

---
+++ sinetext/sinetext.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//because microsoft c compilers do not include a definition of pi
+#ifndef M_PI
+#define M_PI (3.141592654)
+#endif
+
+int main(int argc, char** argv) {
+    int i, nsamps;
+    double samp;
+    double twopi = 2.0 * M_PI;
+    double angleincr;
+    
+    nsamps = 50;
+    angleincr = twopi / nsamps;
+    
+    for (i = 0; i < nsamps; i++) {
+        samp = sin(angleincr * i);
+        fprintf(stdout, "%.8lf\n", samp);
+    } 
+    
+    fprintf(stderr, "done\n");
+    return 0;
+}
