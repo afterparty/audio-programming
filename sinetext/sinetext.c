5872bf4448f0d0c701848452599ddfe2f4cb42f8
afterparty <char6447@yahoo.com>
2019-05-14 15:21:15 +0000

    tuning fork example with args

--- sinetext/sinetext.c
+++ sinetext/sinetext.c
@@ -7,13 +7,22 @@
 #define M_PI (3.141592654)
 #endif
 
+enum { ARG_NAME, ARG_NSAMPS, ARG_FREQ, ARG_SR, ARG_NARGS };
+
 int main(int argc, char** argv) {
     int i, nsamps;
-    double samp;
+    double samp, freq, srate;
     double twopi = 2.0 * M_PI;
     double angleincr;
     
-    nsamps = 50;
+    if (argc != ARG_NARGS) {
+        fprintf(stderr, "Usage: sinetext2 nsamps freq srate\n"); 
+        return 1;
+    }
+    
+    nsamps = atoi(argv[ARG_NSAMPS]);
+    freq = atoi(argv[ARG_FREQ]);
+    srate = atoi(argv[ARG_SR]);
     angleincr = twopi / nsamps;
     
     for (i = 0; i < nsamps; i++) {
